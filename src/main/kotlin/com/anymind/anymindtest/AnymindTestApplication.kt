12d83d240f9e4937b09982408d0ad1d89d3b25f1
tunlaya <tunlaya@ampostech.com>
2021-12-27 21:07:27 +0700

    Init project.

---
+++ src/main/kotlin/com/anymind/anymindtest/AnymindTestApplication.kt
@@ -0,0 +1,11 @@
+package com.anymind.anymindtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnymindTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnymindTestApplication>(*args)
+}
